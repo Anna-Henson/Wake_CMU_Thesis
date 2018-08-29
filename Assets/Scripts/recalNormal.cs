b8301725f0d5ad2ae57d08673cba80b6622909aa
Renee Mei <meiqianye@gmail.com>
2018-08-29 16:09:53 -0700

    add edge detection to discard vertices on extreme depth change

--- Assets/Scripts/recalNormal.cs
+++ Assets/Scripts/recalNormal.cs
@@ -4,14 +4,94 @@ using UnityEngine;
 
 public class recalNormal : MonoBehaviour {
 
+  public float BackgroundSub;
+  public float dScale;
+  public Material material;
+  private float timecounter;
 	// Use this for initialization
 	void Start () {
-    
-	}
+    timecounter = 0.0f;
+  }
 	
 	// Update is called once per frame
-	void Update () {
-    Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
-    mesh.RecalculateNormals();
+	void FixedUpdate () {
+
+    timecounter += Time.deltaTime;
+    if (timecounter > 1.0f)
+    {
+      timecounter = 0.0f;
+      Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+
+
+      Texture2D depthTex = material.GetTexture("_DepthTex") as Texture2D;
+
+      Texture2D prevTex = material.GetTexture("_PrevDepth") as Texture2D;
+      Vector3[] vertices = mesh.vertices;
+      Vector2[] uvs = mesh.uv;
+      int i = 0;
+      while (i < vertices.Length)
+      {
+        bool getRid = false;
+        Vector3 vertex = vertices[i];
+        Vector2 uv = uvs[i];
+        float d = depthTex.GetPixelBilinear(uv.x, uv.y).r;
+        float rs_planeZDist = 3.5f;
+
+        Vector3 camera = new Vector3(0.0f, rs_planeZDist, 0.0f);
+
+        Vector3 projectionVec = (vertex - camera).normalized;
+
+        if (d == 0.0f)
+        {
+          getRid = true;
+          d = prevTex.GetPixelBilinear(uv.x, uv.y).r;
+          if (d == 0.0f)
+          {
+            float xOffset = 0;
+            float yOffset = 0;
+            Vector2 toCenter = new Vector2(0.5f - uv.x, 0.5f - uv.y);
+
+            Vector2 dirVector = toCenter.normalized;
+            for (int k = 1; k < 11; k++)
+            {
+              xOffset = dirVector.x * (k / 30.0f);
+              yOffset = dirVector.y * (k / 30.0f);
+              d = (depthTex.GetPixelBilinear(uv.x + xOffset, uv.y + yOffset).r);
+              if (d != 0.0f && d < BackgroundSub) break;
+            }
+            float dd = 0.0f;
+            for (int j = 1; j < 11; j++)
+            {
+              xOffset = dirVector.x * (j / 30.0f);
+              yOffset = dirVector.y * (j / 30.0f);
+              dd = (depthTex.GetPixelBilinear(uv.x - xOffset, uv.y - yOffset).r);
+              if (dd != 0.0f && d < BackgroundSub) break;
+            }
+
+            if (dd != 0.0f)
+            {
+              if (d != 0.0f)
+              {
+                float[] array = new float[] { d, dd };
+                d = Mathf.Min(array);
+              }
+              else
+              {
+                d = dd;
+              }
+            }
+            if (d == 0)
+            {
+              d = 1.0f;
+            }
+          }
+        }
+        d *= dScale;
+        vertex += d * projectionVec;
+        i++;
+      }
+      mesh.vertices = vertices;
+      mesh.RecalculateNormals();
+    }
   }
 }
--- Assets/Shaders/RSShaderEdge.shader
+++ Assets/Shaders/RSShaderEdge.shader
@@ -1,4 +1,4 @@
-﻿Shader "Custom/RSShader" 
+﻿Shader "Custom/RSShaderEdge" 
 {
 	Properties {
 		_MainTex ("Base (RGB), Alpha (A)", 2D) = "white" {}
@@ -8,6 +8,9 @@
 		_BackgroundSub("Background Point", float) = 0.5
 		_DepthScale("Depth Scale", float) = 1000
 		_Clip("toggle window mask 1 for on 0 for off", float) = 1
+		//Edge Detection Code with Roberts Operators
+		_SampleDistance("Sample Distance", float) = 1.0 //How far away should the samples be apart from each other
+		_Sensitivity("Sensitivity",float) = 1.0 //Sensitivity to depth change
 		
 	}
 	SubShader {
@@ -34,6 +37,7 @@
 		#pragma glsl
 
 		sampler2D _MainTex;
+		half4 _MainTex_TexelSize;
 		sampler2D _DepthTex;
 		sampler2D _PrevDepthTex;
 		sampler2D _BackgroundTex;
@@ -42,7 +46,9 @@
 		float _BackgroundSub;
 		float _DepthScale;
 		float _Clip;
-		float _Magnitude;
+		//Initiate Edge Detection Variables
+		float _Sensitivity;
+		float _SampleDistance;
 
 		struct Input {
 			float2 uv_MainTex;
@@ -51,6 +57,7 @@
 		};
 		
 		void vert(inout appdata_full v, out Input o) {
+			float2 uv = v.texcoord.xy;
 			float4 tex = tex2Dlod(_DepthTex, float4(v.texcoord.xy, 0, 0));
 			UNITY_INITIALIZE_OUTPUT(Input,o);
 			//v.vertex.y -= smoothstep(0,1,tex.r * _ModAmount);
@@ -107,13 +114,30 @@
 			d *= _DepthScale;
 			v.vertex.xyz += d * projectionVec;	
 			o.modelPos = v.vertex.xyz;
-		}
+			}
 		
 		void surf (Input IN, inout SurfaceOutput o) {
 			float2 uv = IN.uv_MainTex;
 			half4 c = tex2D (_MainTex, IN.uv_MainTex);
 			half4 b = tex2D (_DepthTex, IN.uv_MainTex);
-			
+
+			//-----------------Edge Detection-------------------------------------------------------------//
+			half sample1 = tex2D(_DepthTex, uv + _MainTex_TexelSize.xy * half2(1, 1) * _SampleDistance).r;
+			half sample2 = tex2D(_DepthTex, uv + _MainTex_TexelSize.xy * half2(-1, -1) * _SampleDistance).r;
+			half sample3 = tex2D(_DepthTex, uv + _MainTex_TexelSize.xy * half2(-1, 1) * _SampleDistance).r;
+			half sample4 = tex2D(_DepthTex, uv + _MainTex_TexelSize.xy * half2(1, -1) * _SampleDistance).r;
+
+			half edge = 1.0;
+
+			float diffDepth12 = abs(sample1 - sample2) * _Sensitivity;
+			float diffDepth34 = abs(sample3 - sample4) * _Sensitivity;
+
+			int isSameDepth12 = diffDepth12 < 0.1 * sample1;
+			int isSameDepth34 = diffDepth34 < 0.1 * sample3;
+
+			edge = edge * isSameDepth12 * isSameDepth34;
+			//---------------End of Edge Detection---------------------------------------------------------//
+
 			o.Albedo = c.rgb;
 			o.Alpha = 1;
 
@@ -152,6 +176,9 @@
 			if (IN.uv_MainTex.x <= 0.005 || IN.uv_MainTex.y <= 0.005 || IN.uv_MainTex.x >= 0.995 || IN.uv_MainTex.y >= 0.995){
 				o.Alpha = 0;
 			}
+			
+			//Make Steep Depth Change Edge Transparent
+			if (edge < 1.0) o.Alpha = 0;
 			
 		}
 
