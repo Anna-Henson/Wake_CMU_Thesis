4e0e4d7839ec3db21ebf355bfda576bcd84d1421
ahenson1 <henson.anna@gmail.com>
2018-09-17 19:06:33 -0400

    modify lerping of bezier curve; add BeizierDebug

--- Assets/Scripts/LineDataCopyDebug.cs
+++ Assets/Scripts/LineDataCopyDebug.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LineDataCopy : MonoBehaviour {
+public class LineDataCopyDebug : MonoBehaviour {
 
   public BezierCurve bezierCurve;
   public int resolution;
@@ -12,7 +12,8 @@ public class LineDataCopy : MonoBehaviour {
   private float trackerDistance;
   public float lerpAmount;
   public int sign = 1; //should be 1 (forward/player) or -1 (backward/dancer)
-  public float bestDistanceNumber = 2000f;//adjust the range of curve(so that it is not jumpy)
+  public float bestDistance;//adjust the range of curve(so that it is not jumpy)
+  public GameObject lineRendererDancer; 
 
   public void ReachIndex(int index)
   {
@@ -27,7 +28,7 @@ public class LineDataCopy : MonoBehaviour {
   private Vector3 GetClosestPoint(Vector3 position, bool ahead)//Position False
   {
     float bestT = 0;
-    float bestDistance = bestDistanceNumber;
+    float bestDistance = 2000f;
 
     int i = currentIndex;
     BezierPoint p0 = bezierCurve[i];
@@ -62,29 +63,6 @@ public class LineDataCopy : MonoBehaviour {
     return GetClosestPoint(position, false);
   }
 
-  /*public Vector3 GetClosestPoint(Vector3 position, Vector3 currentEndPoint)
-  {
-    float bestT = 0;
-    float bestDistance = 10000f;
-
-    int i = currentIndex;
-    BezierPoint p0 = bezierCurve[i];
-    BezierPoint p1 = bezierCurve[i + 1];
-    for (int j = 1; j < resolution + 1; j++)
-    {
-      float t = (float)j / resolution;
-      Vector3 currentPoint = BezierCurve.GetPoint(p0, p1, t);
-      float distance = Vector3.Distance(position, currentPoint);
-      if (distance < bestDistance)
-      {
-        bestDistance = distance;
-        bestT = t;
-      }
-    }
-    trackerDistance = bestT + trackerLength;
-    Vector3 goalPoint = BezierCurve.GetPoint(p0, p1, trackerDistance);
-    return Vector3.Lerp(currentEndPoint, goalPoint, lerpAmount);
-  }*/
 
   private void PopulateCurve()
   {
@@ -140,21 +118,59 @@ public class LineDataCopy : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-    if (sign == 1)
-    {
-      currentIndex = 0;
-    }
-    else
-    {
-      currentIndex = bezierCurve.pointCount - 1;
-    }
+        
+        lineRendererDancer.GetComponent<LineDataCopy>().enabled = false;
+        if (sign == 1)
+        {
+          currentIndex = 0;
+        }
+        else
+        {
+          currentIndex = bezierCurve.pointCount - 1;
+        }
 
-    lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer = lineRendererDancer.GetComponent<LineRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-      PopulateCurve();
-	}
+      PopulateCurveDebug();
+    }
 
+  
+    private void PopulateCurveDebug()
+    {
+        lineRendererDancer.SetActive(true);
+        lineRenderer = lineRendererDancer.GetComponent<LineRenderer>();
+        List<Vector3> curvePoints = new List<Vector3>();
+
+        for (int i = 7; i > 0; i--)
+        {
+
+            BezierPoint p0 = bezierCurve[i];
+            BezierPoint p1 = bezierCurve[i+sign];
+
+
+            float increment = 1.0f / resolution;
+
+            if (sign == 1)
+            {
+                for (float t = trackerDistance; t < 1; t += increment)
+                {
+                    AddPoint(p0, p1, t, curvePoints);
+                }
+            }
+            else
+            {
+                for (float t = 1; t > 0; t -= increment)
+                {
+                    AddPoint(p0, p1, t, curvePoints);
+                }
+            }
+
+        }
+
+        lineRenderer.positionCount = curvePoints.Count;
+        lineRenderer.SetPositions(curvePoints.ToArray());
+    }
 }
