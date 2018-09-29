d95fe5c284211d800f305a9f09e26e29b2f267af
TasiYokan <guanghao.yang@outlook.com>
2018-09-29 11:35:38 -0400

    Add trigger management framework

---
+++ Assets/Scripts/Refactoring/Trigger.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Trigger : MonoBehaviour,
+    IPrerequisite
+{
+    private bool m_hasTriggered;
+
+    public bool IsSatisfied()
+    {
+        return m_hasTriggered;
+    }
+
+    public Action triggerCallback;
+
+    public List<Trigger> dependencies;
+
+    public void ToTrigger()
+    {
+        foreach (var dependency in dependencies)
+        {
+            if (dependency.IsSatisfied() == false)
+                return;
+        }
+
+        // Do something about the actual content
+        triggerCallback();
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(this.transform.parent.name + " Collide with " + other.name);
+    }
+}
