6f147d5a68564b3b4cc287fe25495d1fea146a9b
ahenson1 <henson.anna@gmail.com>
2018-10-15 20:38:56 -0400

    program beginning; add render texture

---
+++ Assets/PathRecord.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathRecord : MonoBehaviour {
+
+    public GameObject trackerPlayer;
+    public LineRenderer pathRenderer;
+
+    //Dancer's Trackers
+    public GameObject tracker1;
+    public GameObject tracker2;
+
+    private List<Vector3> path = new List<Vector3>() { };
+    private float startTime; 
+    private bool isTracking;
+
+    private IEnumerator DrawPath(List<Vector3> path)
+    {
+        List<Vector3> renderedPath = new List<Vector3>() { };
+
+        foreach (Vector3 point in path)
+        {
+            yield return new WaitForSeconds(0.01f);
+            renderedPath.Add(point);
+            pathRenderer.positionCount = renderedPath.Count;
+            pathRenderer.SetPositions(renderedPath.ToArray());
+            Debug.Log(point);
+        }
+
+        Debug.Log(renderedPath.Count);
+        yield return null;
+    }
+    private void Start()
+    {
+        isTracking = true;
+        startTime = Time.time;
+    }
+
+    void Update () {
+		if (isTracking && Time.time - startTime > 0.05f)
+        {
+            startTime = Time.time;
+            path.Add(trackerPlayer.transform.position);
+        }
+
+        //Stop Tracking Location When Camera Fades out
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            isTracking = false;
+            StartCoroutine(DrawPath(path));
+        }
+	}
+}
---
+++ Assets/Shaders/Path.shader
@@ -0,0 +1,64 @@
+﻿Shader "Custom/Path"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader
+	{
+		Tags { "RenderType"="TransparentCutoff" "Queue" = "Transparent" }
+		ZTest LEqual
+		ZWrite Off
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		AlphaToMask On
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, i.uv);
+				col.a = max(0, (col.r + col.b + col.g) / 3);
+				// apply fog
+				UNITY_APPLY_FOG(i.fogCoord, col);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
